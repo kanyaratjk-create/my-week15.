e3791d592fbcd7fbac28e5de9a7b2c37f70ad7af
kanyarat2004 <kanyarat.jk@rmuti.ac.th>
2026-09-21 16:16:47 +0700

    5555

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+ // 1. นำเข้า Trait
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable; // 2. เรียกใช้ HasApiTokens ภายในคลาส
 
     /**
      * The attributes that are mass assignable.
